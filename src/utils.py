ef5bf92f6f0246b99675ead0ab60ed9728a5d6b4
rjrandria-lang <rojoni.randria@gmail.com>
2026-03-16 05:41:41 +0100

    Add utility functions to src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+def utility_function_one():
+    """Description of utility function one."""
+    pass
+
+
+def utility_function_two():
+    """Description of utility function two."""
+    pass
+
+
+def utility_function_three():
+    """Description of utility function three."""
+    pass
